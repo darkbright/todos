dd19d7ef3abac618c1082b0ce3735232aa7a0688
darkbright <foxtori8946@gmail.com>
2022-07-13 18:05:46 +0900

    스프링 MVC 정적 자원 연결

--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,5 +1,9 @@
 package todoapp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -9,7 +13,34 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
  *
  * @author springrunner.kr@gmail.com
  */
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public WebMvcConfiguration() {
+        logger.debug("스프링 MVC 설정자 생성");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 서블릿 컨텐스트 경로에서 정적 자원 제공
+//        registry.addResourceHandler("/assets/**").addResourceLocations("assets/");
+
+        // 파일 경로에서 정적 자원 제공
+//        registry.addResourceHandler("/assets/**")
+//                .addResourceLocations("file:/workspace/todos/files/assets/");
+
+        // 클래스패스 경로에서 정적 자원 제공
+//        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:assets/");
+
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations(
+                        "assets/",
+                        "file:/workspace/todos/files/assets/",
+                        "classpath:assets/"
+                );
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
