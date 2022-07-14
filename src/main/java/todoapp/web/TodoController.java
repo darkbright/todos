d25aef6114f3c92e357a7e141bc6225a96e72ea0
darkbright <foxtori8946@gmail.com>
2022-07-14 09:26:41 +0900

    스프링MVC 사이트 정보 Environment 활용

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,15 +1,39 @@
 package todoapp.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import todoapp.web.model.SiteProperties;
+
+import javax.validation.Valid;
 
 @Controller
 public class TodoController {
 
-    @RequestMapping("/todos")
-    public ModelAndView todos() {
-        return new ModelAndView("todos");
+    private Environment environment;
+    private String siteAuthor;
+
+    public TodoController(Environment environment, @Value("${site.author}") String siteAuthor) {
+        this.environment = environment;
+        this.siteAuthor = siteAuthor;
     }
 
+    @RequestMapping("/todos")
+    public ModelAndView todos() {
+        SiteProperties site = new SiteProperties();
+//        site.setAuthor(environment.getProperty("site.author"));
+        site.setAuthor(siteAuthor);
+        site.setDescription("스프링 MVC");
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("site", site);
+        mav.setViewName("todos");
+
+        return mav;
+    }
 }
